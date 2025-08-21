e91094735bac66740a6ea9984038361bc029c20a
AbhishekHalkeri <abhishekhalkeri@gmail.com>
2025-08-21 15:53:53 +0530

    added blog

--- web/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Contact(models.Model):
-    first_name=models.CharField(max_length=15)
-    last_name=models.CharField(max_length=15)
-    email=models.EmailField()
-    content=models.TextField(max_length=500)
--- web/app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .forms import ContactForm
-# Create your views here.
-def index(request):
-    con=ContactForm()
-    return render(request,'index.html',{'form':con})
